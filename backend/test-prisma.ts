9077443400b19d531093d8e90afa78b8a5ae82d1
Kaycee276 <kizuaba@gmail.com>
2026-07-04 15:01:43 +0100

    chore: remove obsolete prisma test files and update build exclude configuration

--- backend/test-prisma.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient({ log: ['info'] });
-prisma.$connect().then(() => {
-  console.log('Connected successfully!');
-  process.exit(0);
-}).catch(e => {
-  console.error(e);
-  process.exit(1);
-});
